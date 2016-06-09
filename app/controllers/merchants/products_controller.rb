46ccac76f4c4e5a823d84fb835c219ec6f38ed36
Abdurrahman uymaz <appie_u_@hotmail.com>
2016-06-09 12:24:54 +0200

    add merchants

---
+++ app/controllers/merchants/products_controller.rb
@@ -0,0 +1,22 @@
+class Merchant::ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only:[ :index, :show ]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params.require(:product).permit(:name))
+    @product.save!
+    redirect_to product_path(@product)
+  end
+
+end
